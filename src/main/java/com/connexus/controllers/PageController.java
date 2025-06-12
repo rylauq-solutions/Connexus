b7a33f10cabd1a62eccf8bb07b437af68c96a831
Anubhav Goel <anubhavgoel0520@hotmail.com>
2025-06-12 20:35:41 +0530

    Add: Connected Tailwind

--- src/main/java/com/connexus/controllers/PageController.java
+++ src/main/java/com/connexus/controllers/PageController.java
@@ -6,12 +6,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-    
+
     @RequestMapping("/home")
     public String home(Model model) {
         System.out.println("Home Page Handler");
-        model.addAttribute("message", "Welcome to the Home Page!");
         model.addAttribute("name", "Connexus Application");
+        model.addAttribute("message", "Welcome to the Home Page!");
+        model.addAttribute("githubRepo", "https://github.com/er-YOUR_NEW_NAME/Connexus");
         return "home";
+    }
+
+    // About Route
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        System.out.println("About Page Loading");
+        return "about";
+    }
+
+    // Services Route
+    @RequestMapping("/services")
+    public String servicesPage(Model model) {
+        System.out.println("Services Page Loading");
+        return "services";
     }
 }
